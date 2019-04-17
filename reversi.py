c0e2ba6155c4ae6cdca852f55f04686c78b016ee
William Tang <galaxyking0419@gmail.com>
2019-04-17 16:33:31 +1000

    Apply optimization patch

--- reversi.py
+++ reversi.py
@@ -1,21 +1,14 @@
 # Copyright (c) 2019, William TANG <galaxyking0419@gmail.com>
+# necessary modules
 from copy import deepcopy
+from time import sleep
+from random import choice
 
-# frequent used varibles
-columns_map = {
-	'a' : 1,
-	'b' : 2,
-	'c' : 3,
-	'd' : 4,
-	'e' : 5,
-	'f' : 6,
-	'g' : 7,
-	'h' : 8
-}
-
+# global varibles
 players_map = {
-	1 : 'B',
-	2 : 'W'
+	0: '-',
+	1: 'B',
+	2: 'W'
 }
 
 direction_list = [
@@ -29,6 +22,12 @@ direction_list = [
 	[-1, -1]
 ]
 
+random_sentences = [
+	"Emmm, interesting...",
+	"Let me think for a while...",
+	"Hold my beer",
+	"Aha! You are dead!"
+]
 
 # custom functions
 def your_oppenent(player):
@@ -53,29 +52,25 @@ def ai_place(board, player):
 		score_list.append([counter, score(next_state(board_next, player, opt)[0])[1] - current_score])
 		counter += 1
 
-	# use insertion sort due to better performance
-	for i in range(1, len(score_list)):
-		for j in range(i, 0, -1):
-			if score_list[j - 1][1] < score_list[j][1]:
-				score_list[j], score_list[j - 1] = score_list[j - 1], score_list[j]
-			else:
-				break
-
+	# sort in descending order
+	sorted(score_list, key=lambda score: score_list[1], reverse=True)
 	index = score_list[0][0]
 	return valid_opts[index]
-	
+
 
 def prompt_to_place(board, player):
 	pos = input("(%s) Enter the position to place your disk: " % (players_map[player]))
+
 	#if the player want to quit, exit the program with code 1
 	if pos == 'q':
-		print("Thanks for playing the reversi.")
-		exit(1)
+		print("Thanks for playing my Reversi.")
+		exit(0)
+
 	# phrase the position in the list
 	pos = position(pos)
 
 	if pos not in valid_moves(board, player):
-		print("invalid move")
+		print("Come on, don't mess up with me.")
 		return prompt_to_place(board, player)
 	return pos
 
@@ -115,13 +110,10 @@ def new_board():
 def print_board(board):
 	for i in range(8):
 		# print row number before each row
-		print("%d |" % (i + 1), end = ' ')
+		print("%d" % (i + 1), end = ' | ')
 
 		for j in range(8):
-			if board[i][j] == 0:
-				print("-", end = ' ')
-			else:
-				print(players_map[board[i][j]], end = ' ')
+			print(players_map[board[i][j]], end = ' ')
 
 		# print the socre board on the right	
 		if i == 3:
@@ -151,23 +143,12 @@ def enclosing(board, player, pos, direct):
 	row = pos[0] + direct[0]
 	column = pos[1] + direct[1]
 
-	# if the index is out of range return false
-	if not on_board([row, column]):
-		return False
-
 	# if there is a oppenent's chess beside your chess, continue checking
-	if board[row][column] == your_oppenent(player):
-		while on_board([row, column]):
-			# if the current position is empty, return false
-			if board[row][column] == 0:
-				return False
-			if board[row][column] == player:
-				return True
-			row += direct[0]
-			column += direct[1]
-	# if not, return false to accelerant the function
-	else:
-		return False
+	while on_board([row, column]) and board[row][column] == your_oppenent(player):
+		if on_board([row+direct[0], column+direct[1]]) and board[row+direct[0]][column+direct[1]] == player:
+			return True
+		row += direct[0]
+		column += direct[1]
 	# default to false
 	return False
 
@@ -184,12 +165,14 @@ def valid_moves(board, player):
 					# exit the direction loop if find one eligible position
 					break
 	return moves_list
-	
 
 
 def next_state(board, player, pos):
-	board[pos[0]][pos[1]] = player
+	# if there is no valid move for both of players
+	if valid_moves(board, 1) == [] and valid_moves(board, 2) == []:
+		return board, 0
 
+	board[pos[0]][pos[1]] = player
 	for direction in direction_list:
 		# copy the value of direction list in case of varible changes
 		direction_next = direction[:]
@@ -199,14 +182,20 @@ def next_state(board, player, pos):
 				board[pos[0] + direction_next[0]][pos[1] + direction_next[1]] = player
 				direction_next[0] += direction[0]
 				direction_next[1] += direction[1]
-
-	# no valid move for both of players
-	if valid_moves(board, 1) == [] and valid_moves(board, 2) == []:
-		return board, 0
 	return board, your_oppenent(player)
 
 
 def position(pos):
+	columns_map = {
+		'a': 1,
+		'b': 2,
+		'c': 3,
+		'd': 4,
+		'e': 5,
+		'f': 6,
+		'g': 7,
+		'h': 8
+	}
 	if len(pos) == 2 and pos[0] in "abcdefgh" and pos[1] in "1234567890":
 		return [(int(pos[1]) - 1), (columns_map[pos[0]] - 1)]
 	return None
@@ -217,8 +206,8 @@ def run_two_players():
 	player = 1
 	game_board = new_board()
 
-	# infinite loop to keep the game going
-	while True:
+	# while the player is 1 or 2 keep the game going
+	while player != 0:
 		print_board(game_board)
 
 		# pos must be a valid position
@@ -226,67 +215,35 @@ def run_two_players():
 
 		game_board, player = next_state(game_board, player, pos)
 
-		if player == 0:
-			finish_game(game_board)
+	# finish game once the player is 0
+	finish_game(game_board)
 
 
 def run_single_players():
 	# initialize the borad and player
-	counter = 1
 	player = 1
 	game_board = new_board()
 
-	# infinite loop to keep the game going
-	while True:
+	# while the player is 1 or 2 keep the game going
+	while player != 0:
 		print_board(game_board)
 
 		# player place first
-		if counter % 2 == 1:
+		if player == 1:
 			pos = prompt_to_place(game_board, player)
 		else:
+			print(choice(random_sentences))
+			sleep(2)
 			pos = ai_place(game_board, player)
 
 		game_board, player = next_state(game_board, player, pos)
+	# finish game once the player is 0
+	finish_game(game_board)
 
-		if player == 0:
-			finish_game(game_board)
-		
-		counter += 1
-
-
-# test zone
-# uncomment them if you want to try examples in the assignment PDF
-## (a)
-#print(score(new_board()))
-## (b)
-#print(enclosing(new_board(), 1, (4, 5), (0, -1)))
-#print(enclosing(new_board(), 1, (4, 5), (1, 1)))
-## (c)
-#print_board(next_state(new_board(), 1, (4, 5))[0])
-#print(next_state(new_board(), 1, (4, 5))[1])
-## (d)
-#print(valid_moves(next_state(new_board(), 1, (4, 5))[0], 2))
-## (e)
-#print(position("e3"))
-#print(position("Genghis Khan"))
-# two players game & single player game
-run_two_players()
-#run_single_players()
-
-# custom functions test zone
-## ai_place()
-#print(ai_place(new_board(), 2))
-
-
-# The actual game starts from here, uncomment them if you want to play
-#mode = input(
-#"Welcome to the reversi!\n\
-#enter 1 to play in single player mode\n\
-#enter 2 to play in two players mode\n\
-#Choose your mode: "
-#)
-#
-#if mode == '1':
-#	run_single_players()
-#elif mode == '2':
-#	run_two_players()
+# main program
+if __name__ == "__main__":
+	mode = input("Welcome to the reversi!\nenter 1 to play in single player mode\nenter 2 to play in two players mode\nChoose your mode: ")
+	if mode == '1':
+		run_single_players()
+	elif mode == '2':
+		run_two_players()
